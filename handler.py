4426745cd81d51f9645b0589afd95c3683d43043
Jack <jackn926@live.co.uk>
2020-05-04 15:43:33 +0100

    added data for cw and indeed

--- handler.py
+++ handler.py
@@ -16,7 +16,7 @@ class JobSite(object):
        return extracted_jobs
 
    @classmethod
-   def insert_into_db(obj, jobs):
+   def insert_into_db(cls, jobs):
        """
        inserts jobs to aws dynamodb
        """
@@ -25,13 +25,12 @@ class JobSite(object):
        condition = 'attribute_not_exists(js.jobs_raw.id)'
        for job in jobs:
            dynamodb.put_item(TableName=table_name,
-                             Item=job,
-                             ConditionExpression=condition)
+                             Item=job)
 
 class Reed(JobSite):
     url = "https://www.reed.co.uk/jobs/junior-python-jobs-in-london?sortby=DisplayDate"
     job_block = "job-result"
-    def extract_job_details(obj, job):
+    def extract_job_details(self, job):
         detail = {
             "id": {"S": job.find(class_="job-result-anchor")['id']},
             "title": {"S": job.find('h3').get_text().strip()},
@@ -50,20 +49,48 @@ class Reed(JobSite):
         return detail
 
 class Indeed(JobSite):
-    url = ""
-    job_block = ""
+    url = "https://www.indeed.co.uk/jobs?q=Junior+Python&l=London&radius=10&start=0&limit=100"
+    job_block = "jobsearch-SerpJobCard unifiedRow row result"
 
     def extract_job_details(obj, job):
-        detail = {}
+        detail = {"id": {"S": job.find(class_="recJobLoc")['id'][10:]},
+                  "title": {"S": job.find(class_="jobtitle turnstileLink").get_text().strip()},
+                  "location": {"S": job.find(class_="location accessible-contrast-color-location").get_text().strip()},
+                  "salary": {
+                      "S": job.find(class_="salaryText").get_text().strip() if job.find(class_="salaryText") else "null"},
+                  "contract": {"S": "null"},
+                  "company": {"S": job.find(class_="company").get_text().strip()},
+                  "age": {"S": job.find(class_="date").get_text().strip()},
+                  "link": {"S": "https://www.indeed.co.uk" + job.find("a")["href"]},
+                  "date": {"S": date.today().strftime("%d/%m/%Y")},
+                  "agency": {"S": "Indeed"},
+                  "summary": {"S": job.find(class_="summary").get_text().strip()},
+                  "interested": {"S": "TBC"},
+                  "reviewed": {"S": "N"},
+                  }
 
         return detail
 
 class Cw(JobSite):
-    url = ""
-    job_block = ""
+    url = "http://www.cwjobs.co.uk/jobs/junior-python/in-london?radius=10&s=recentsearch&Sort=3"
+    job_block = "job"
 
     def extract_job_details(obj, job):
-        detail = {}
+        detail = {
+            "id": {"S": job.find("a")["href"][-8:]},
+            "title": {"S": job.find("h2").get_text()},
+            "location": {"S": job.find(class_="location").find("span").get_text().strip()},
+            "salary": {"S": job.find(class_="salary").get_text().replace("£", "")},
+            "contract": {"S": job.find(title="employment type").get_text()},
+            "company": {"S": job.find(title="hiring organization").get_text().strip()},
+            "age": {"S": job.find(title="posted date").get_text().strip()},
+            "link": {"S": job.find("a")["href"]},
+            "date": {"S": date.today().strftime("%d/%m/%Y")},
+            "agency": {"S": "CW Jobs"},
+            "summary": {"S": job.find(class_="job-intro").get_text()},
+            "interested": {"S": "TBC"},
+            "reviewed": {"S": "N"},
+        }
 
         return detail
 
@@ -72,13 +99,30 @@ class JobSiteFactory():
    def create_job_search(self, job_site):
       return globals()[job_site]()
 
-job_obj = JobSiteFactory()
-button = ['Cw', 'Reed', 'Indeed']
+def scrape(event, context):
+    """
+    event handler - entry point for function
+    """
+    job_obj = JobSiteFactory()
+    job_sites = ['Cw', 'Reed', 'Indeed',]
 
-jobs = job_obj.create_job_search('Reed').get_jobs()
-JobSite.insert_into_db(jobs)
+    for site in job_sites:
+        try:
+            jobs = job_obj.create_job_search(site).get_jobs()
+            JobSite.insert_into_db(jobs)
+            print(f"COMPLETE: scrape from {site} successful")
+        except:
+            print(f"ERROR: scrape from {site} unsuccessful")
+
+    return {
+        'message': "job scrape complete"
+    }
+
 
 """
 for b in button:
    print button_obj.create_button(b).get_jobs()
 """
+
+if __name__ == "__main__":
+    scrape("foo", "bar")
